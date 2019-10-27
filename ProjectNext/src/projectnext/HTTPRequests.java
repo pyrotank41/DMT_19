e7dad4f22f72462259cd837c78ba398047ce7968
Daniel Johnston <daniel.august.j@gmail.com>
2019-10-27 05:05:32 -0500

    Add files via upload

---
+++ ProjectNext/src/projectnext/HTTPRequests.java
@@ -0,0 +1,60 @@
+package projectnext;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.*;
+
+public class HTTPRequests 
+{
+    private URL url;
+    private HttpURLConnection connection;
+    final private String SERVER_ADDRESS = "http://100.79.2.35:5000/";
+    
+    // Receive json file
+    public String SendRequest(String parameters) throws MalformedURLException, IOException
+    {
+        String dataAddress = SERVER_ADDRESS + parameters;
+        url = new URL(dataAddress);
+        connection = (HttpURLConnection) url.openConnection();     
+        connection.setRequestMethod("GET");   
+        //connection.setDoOutput(true); 
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+    
+       // DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+        
+        /*
+        outputStream.writeBytes(parameters);
+        outputStream.flush();
+        outputStream.close();
+*/
+        int status = connection.getResponseCode();
+        Reader streamReader = null;
+        
+        if (status > 299) 
+        {
+            streamReader = new InputStreamReader(connection.getErrorStream());
+            return null;
+        }
+        else 
+        {
+            streamReader = new InputStreamReader(connection.getInputStream());
+        }
+        
+        BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream())); 
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+
+        while((inputLine = input.readLine()) != null)
+        {
+            content.append(inputLine);
+        }
+        
+        input.close();
+        connection.disconnect();   
+        
+        return content.toString();
+    }    
+}
---
+++ ProjectNext/src/projectnext/LocationInformation.java
@@ -0,0 +1,70 @@
+package projectnext;
+
+import java.text.DecimalFormat;
+
+public class LocationInformation 
+{
+    public String   PropertyID,
+                    Name,
+                    Address,
+                    City,
+                    State,
+                    Market,
+                    SubMarket,
+                    MicroMarket,
+                    Class,
+                    YearBuilt,
+                    BuildingSize,
+                    Stories,
+                    PropertyType,
+                    LeasingCompany,
+                    Manager,
+                    IsPrime,
+                    SubwayService,
+                    WalkScore,
+                    TransitScore,
+                    CrimeGrade,
+                    ExpansionPotential,
+                    SalesPrice,
+                    Buyer,
+                    Seller,
+                    FirstYearRent,
+                    FloorNum,
+                    FloorPrice;
+    public LocationInformation()
+    {
+        
+    }
+    public String GetDetails()
+    {
+        String Description = "";
+        
+        Description += String.format
+        (
+                "%s is located at %s, %s %s", 
+                Name, Address, City, State
+        );
+        
+        if(Market != null)
+        {
+            Description += " in the ";
+            if(SubMarket != null)
+            {
+                Description += SubMarket + " Submarket of the "; 
+            }
+            
+                Description += Market + "Market. ";
+        }
+        
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        BuildingSize = decimalFormat.format(Double.valueOf(BuildingSize));
+        
+        Description += String.format
+                (
+                "This property is a Class %s %s building, built in %s, standing %s stories tall with a total size of %s SQ ft. ",
+                Class, PropertyType, YearBuilt, Stories, BuildingSize
+                );
+        
+        return Description;
+    }
+}
